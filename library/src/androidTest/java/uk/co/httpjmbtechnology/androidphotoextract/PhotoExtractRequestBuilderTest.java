93caa588c7d297d2c85aa6c9c546e96998f516ab
James Baster <james@jarofgreen.co.uk>
2016-03-11 13:12:44 +0000

    Test!

---
+++ library/src/androidTest/java/uk/co/httpjmbtechnology/androidphotoextract/PhotoExtractRequestBuilderTest.java
@@ -0,0 +1,22 @@
+package uk.co.httpjmbtechnology.androidphotoextract;
+
+
+import android.test.AndroidTestCase;
+
+public class PhotoExtractRequestBuilderTest  extends AndroidTestCase {
+
+	public void testDefault() {
+		PhotoExtractRequestBuilder photoExtractRequestBuilder = new PhotoExtractRequestBuilder();
+
+		PhotoExtractRequest photoExtractRequest = photoExtractRequestBuilder.build();
+
+		assertFalse(photoExtractRequest.isReturnDimensions());
+		assertFalse(photoExtractRequest.isReturnEXIF());
+		assertFalse(photoExtractRequest.isReturnFileName());
+		assertFalse(photoExtractRequest.isReturnMIMEType());
+		assertFalse(photoExtractRequest.isReturnRawDebugInformation());
+		assertFalse(photoExtractRequest.isReturnThumbnail());
+	}
+
+}
+
