4ac108e66deabebb752f4d2dc6d6b143e53d9d06
James Baster <james@jarofgreen.co.uk>
2016-03-11 16:25:51 +0000

    More basic tests

--- library/src/androidTest/java/uk/co/jmbtechnology/androidphotoextract/PhotoExtractRequestBuilderTest.java
+++ library/src/androidTest/java/uk/co/jmbtechnology/androidphotoextract/PhotoExtractRequestBuilderTest.java
@@ -20,5 +20,89 @@ public class PhotoExtractRequestBuilderTest  extends AndroidTestCase {
 		assertFalse(photoExtractRequest.isReturnThumbnail());
 	}
 
+	public void testDimensions() {
+		PhotoExtractRequestBuilder photoExtractRequestBuilder = new PhotoExtractRequestBuilder();
+		photoExtractRequestBuilder.setReturnDimensions(true);
+
+		PhotoExtractRequest photoExtractRequest = photoExtractRequestBuilder.build();
+
+		assertTrue(photoExtractRequest.isReturnDimensions());
+		assertFalse(photoExtractRequest.isReturnEXIF());
+		assertFalse(photoExtractRequest.isReturnFileName());
+		assertFalse(photoExtractRequest.isReturnMIMEType());
+		assertFalse(photoExtractRequest.isReturnRawDebugInformation());
+		assertFalse(photoExtractRequest.isReturnThumbnail());
+	}
+
+	public void testEXIF() {
+		PhotoExtractRequestBuilder photoExtractRequestBuilder = new PhotoExtractRequestBuilder();
+		photoExtractRequestBuilder.setReturnEXIF(true);
+
+		PhotoExtractRequest photoExtractRequest = photoExtractRequestBuilder.build();
+
+		assertFalse(photoExtractRequest.isReturnDimensions());
+		assertTrue(photoExtractRequest.isReturnEXIF());
+		assertFalse(photoExtractRequest.isReturnFileName());
+		assertFalse(photoExtractRequest.isReturnMIMEType());
+		assertFalse(photoExtractRequest.isReturnRawDebugInformation());
+		assertFalse(photoExtractRequest.isReturnThumbnail());
+	}
+
+	public void testFilename() {
+		PhotoExtractRequestBuilder photoExtractRequestBuilder = new PhotoExtractRequestBuilder();
+		photoExtractRequestBuilder.setReturnFileName(true);
+
+		PhotoExtractRequest photoExtractRequest = photoExtractRequestBuilder.build();
+
+		assertFalse(photoExtractRequest.isReturnDimensions());
+		assertFalse(photoExtractRequest.isReturnEXIF());
+		assertTrue(photoExtractRequest.isReturnFileName());
+		assertFalse(photoExtractRequest.isReturnMIMEType());
+		assertFalse(photoExtractRequest.isReturnRawDebugInformation());
+		assertFalse(photoExtractRequest.isReturnThumbnail());
+	}
+
+	public void testMIMEType() {
+		PhotoExtractRequestBuilder photoExtractRequestBuilder = new PhotoExtractRequestBuilder();
+		photoExtractRequestBuilder.setReturnMIMEType(true);
+
+		PhotoExtractRequest photoExtractRequest = photoExtractRequestBuilder.build();
+
+		assertFalse(photoExtractRequest.isReturnDimensions());
+		assertFalse(photoExtractRequest.isReturnEXIF());
+		assertFalse(photoExtractRequest.isReturnFileName());
+		assertTrue(photoExtractRequest.isReturnMIMEType());
+		assertFalse(photoExtractRequest.isReturnRawDebugInformation());
+		assertFalse(photoExtractRequest.isReturnThumbnail());
+	}
+
+	public void testRawDebugInfo() {
+		PhotoExtractRequestBuilder photoExtractRequestBuilder = new PhotoExtractRequestBuilder();
+		photoExtractRequestBuilder.setReturnRawDebugInformation(true);
+
+		PhotoExtractRequest photoExtractRequest = photoExtractRequestBuilder.build();
+
+		assertFalse(photoExtractRequest.isReturnDimensions());
+		assertFalse(photoExtractRequest.isReturnEXIF());
+		assertFalse(photoExtractRequest.isReturnFileName());
+		assertFalse(photoExtractRequest.isReturnMIMEType());
+		assertTrue(photoExtractRequest.isReturnRawDebugInformation());
+		assertFalse(photoExtractRequest.isReturnThumbnail());
+	}
+
+	public void testThumbnail() {
+		PhotoExtractRequestBuilder photoExtractRequestBuilder = new PhotoExtractRequestBuilder();
+		photoExtractRequestBuilder.setReturnThumbnail(true);
+
+		PhotoExtractRequest photoExtractRequest = photoExtractRequestBuilder.build();
+
+		assertFalse(photoExtractRequest.isReturnDimensions());
+		assertFalse(photoExtractRequest.isReturnEXIF());
+		assertFalse(photoExtractRequest.isReturnFileName());
+		assertFalse(photoExtractRequest.isReturnMIMEType());
+		assertFalse(photoExtractRequest.isReturnRawDebugInformation());
+		assertTrue(photoExtractRequest.isReturnThumbnail());
+	}
+
 }
 
